ac17955d0eddd76660920069350c0b610a20cd90
Lucrecia Fidanza <lucreciafidanza@gmail.com>
2017-09-18 18:28:17 -0300

    Puntos 5 y 6

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,4 +6,9 @@ function guess() {
     //add functionality to guess function here
 }
 
-//implement new functions here
+function setHiddenFields(){
+  answer.value = Math.floor(Math.random()*10000).toString();
+  while(answer.value.length<4){
+    answer.value = "0" + answer.value;
+  }
+}
